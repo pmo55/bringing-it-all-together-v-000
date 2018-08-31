bc89c86c9254fd365b0a146657913e821339b38d
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-31 16:56:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ?
     SQL
-    DB[:conn].execute(sql, self.name).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
