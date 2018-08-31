26fa17e03fe0fb03964d7c44eb67e9a59b4d2f55
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-31 16:44:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      dog = Self.create(name:, breed:)
+      dog = Self.create(name: name, breed: breed)
     end
   end
   def update
