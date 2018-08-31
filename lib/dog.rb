bec58190c91628d4ac0e248eb00be70d742811af
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-31 16:37:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
     SET name = ?, breed = ?
     WHERE id = ?
     SQL
-    DB[:conn].execute(sql, self.id, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
   
